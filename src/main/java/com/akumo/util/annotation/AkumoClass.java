d890054ce2caa10c800c88bcd4a1453109b1bd32
apedrina <pedrina.alisson@gmail.com>
2020-08-11 23:53:20 +0200

    initial

---
+++ src/main/java/com/akumo/util/annotation/AkumoClass.java
@@ -0,0 +1,9 @@
+package com.akumo.util.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface AkumoClass {
+}
---
+++ src/main/java/com/akumo/util/annotation/AkumoObject.java
@@ -0,0 +1,10 @@
+package com.akumo.util.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface AkumoObject {
+    String name() default "";
+}
